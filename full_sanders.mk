8638a0948ae9047c70950e35f0c8f66d908161a4
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-20 00:20:34 -0200

    sanders: Disable dexpreopt for some modules

--- full_sanders.mk
+++ full_sanders.mk
@@ -20,6 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from potter device
 $(call inherit-product, device/motorola/sanders/device.mk)
+$(call inherit-product, device/motorola/sanders/dexpreopt.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
