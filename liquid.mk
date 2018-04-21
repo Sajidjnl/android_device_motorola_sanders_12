bf0f8b15a4ec6f2a95750ecf5d108c8d4a470ed8
syed sajid <syedsajid341@gmail.com>
2018-04-21 03:36:58 +0000

    CleanUp Dir

--- liquid.mk
+++ liquid.mk
@@ -18,8 +18,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/sanders/full_sanders.mk)
 
-# Inherit some common CM stuff.
-
 # Inherit some common Liquid stuff.
 
 $(call inherit-product, vendor/liquid/config/common_full_phone.mk)
