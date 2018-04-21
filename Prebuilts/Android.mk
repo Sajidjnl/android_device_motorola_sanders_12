bcd046176ed7724e5d3ce5c3b9c7264944557f0a
syed sajid <syedsajid341@gmail.com>
2018-04-21 11:26:45 +0000

    Removed GoogleCamera

--- Prebuilts/Android.mk
+++ Prebuilts/Android.mk
@@ -1,21 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-<<<<<<< HEAD
-LOCAL_MODULE := GoogleCamera
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := GoogleCamera.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
-LOCAL_OVERRIDES_PACKAGES := Camera2 Snap
-include $(BUILD_PREBUILT)
-
-
-include $(CLEAR_VARS)
-=======
->>>>>>> parent of f3486c0... Added GoogleCamera
 LOCAL_MODULE := RetroMusicPlayer
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := RetroMusicPlayer.apk
